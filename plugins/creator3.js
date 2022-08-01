07de98082edc2e6b833488a6a4202ae486028ffb
alpinnio <107537249+alpinnio@users.noreply.github.com>
2022-08-01 20:11:24 +0700

    Update creator3.js

--- plugins/creator3.js
+++ plugins/creator3.js
@@ -9,7 +9,7 @@ contextInfo: { externalAdReply :{
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'DEVELOPER BOT: FANGZ',          previewType: 0,
+                        body: 'DEVELOPER BOT: LUXXY',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
                         sourceUrl: snh
                       }}
